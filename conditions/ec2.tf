1e5b78fc1b83a3435e08afc29855f9625c13c3bb
Shankar-codes <harikainteriors@gmail.com>
2026-05-11 17:54:43 +0530

    conditions

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "Terraform" {
   ami           = var.ami_id
-  instance_type = var.instance_type == "dev" ? var.instance_type : "t3.micro"
+  instance_type = var.instance_type == "prod" ? var.instance_type : "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = var.ec2_tags
 }

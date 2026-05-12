16162e4a7c4776b14f4eec39ebecde61e3bf96ef
Shankar-codes <harikainteriors@gmail.com>
2026-05-12 17:51:16 +0530

    provisioner

---
+++ provisioners/ec2.tf
@@ -0,0 +1,64 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = "terraform-1"
+        Terraform = "true"
+    }
+
+    provisioner "local-exec"{
+      command = "echo ${self.private_ip} > inventory"
+      on_failure = continue
+    }
+
+    provisioner "local-exec"{
+      command = "echo Instance is destroyed"
+      when    = destroy
+    }
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+      ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stopped nginx server' "
+      ]
+      when = destroy
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound and outbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
